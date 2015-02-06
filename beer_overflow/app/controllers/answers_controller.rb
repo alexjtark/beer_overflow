1002f6620ce43101233c5dad4fe4bcd66810621b
Kevin Perumal <kevin.perumal@gmail.com>
2015-02-06 08:59:31 -0500

    add answer new and create controller actions

--- beer_overflow/app/controllers/answers_controller.rb
+++ beer_overflow/app/controllers/answers_controller.rb
@@ -1,14 +1,20 @@
-class PagesController < ApplicationController
+class AnswersController < ApplicationController
 
 def create
   answer = Answer.create(content: params[:answer][:content], responder_id: sessions[:id], question_id: params[:id])
 
-  redirect_to question
+  redirect_to :controller => 'question', :action => 'show', :id => params[:id]
 
 end
 
 def new
   @answer = Answer.new
+end
+
+def show
+  @answer = Answer.find(params[:id])
+  @question = Question.find(@answer.question_id)
+  @responder = User.find(@answer.responder_id)
 end
 
 def edit
