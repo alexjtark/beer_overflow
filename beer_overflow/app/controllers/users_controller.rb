55eb74c17aaed1bd703d8274b0154effae2e0bbe
Ashley Baldwin-Hunter <ashleybaldwin-hunter@Ashleys-MacBook-Air.local>
2015-02-05 23:51:11 -0500

    complete create route for new user

--- beer_overflow/app/controllers/users_controller.rb
+++ beer_overflow/app/controllers/users_controller.rb
@@ -9,6 +9,13 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    if @user.save
+      session[:id] = @user.id
+      redirect_to root_path
+    else
+      @errors = @user.errors.messages
+      redirect_to :back
+    end
   end
 
   private
