db97c89f8050930b4da434912fcdea7c82d1eb41
alexjtark <alex.tarkowski@gmail.com>
2015-02-05 21:19:43 -0500

    add tests to test for valid question

--- beer_overflow/spec/models/question_spec.rb
+++ beer_overflow/spec/models/question_spec.rb
@@ -5,3 +5,25 @@ describe 'Initial Test' do
     expect(true).to eq true
   end
 end
+
+describe Question do
+  it 'should return a valid question' do
+    question = Question.new(
+      title: "question title",
+      content: "question content")
+    expect(question).to be_valid
+  end
+
+  it 'should return an error if title is blank' do
+    question = Question.new(content: "question content")
+    question.valid?
+    expect(question.errors[:title]).to include("can't be blank")
+  end
+
+  it 'should return an error if content is blank' do
+    question = Question.new(title: "question title")
+    question.valid?
+    expect(question.errors[:content]).to include("can't be blank")
+  end
+end
+
