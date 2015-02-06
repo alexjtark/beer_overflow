83f0b566fe6bf7c54edc12df19f7914212c410d8
Amar Singh <ummerr@gmail.com>
2015-02-06 12:22:44 -0500

    minor spec change

--- beer_overflow/spec/models/comment_spec.rb
+++ beer_overflow/spec/models/comment_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe Comment do
   it 'should return a valid comment' do
     comment = Comment.new(
-      content: "comment content")
+      content: "comment content commenting on content")
     expect(comment).to be_valid
   end
 
