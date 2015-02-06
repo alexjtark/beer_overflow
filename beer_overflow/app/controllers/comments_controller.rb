585c04928abfa28ebf5ec4df97ef0db169572140
Amar Singh <ummerr@gmail.com>
2015-02-06 11:18:58 -0500

    added private comment methods for strong params and setting a comment

--- beer_overflow/app/controllers/comments_controller.rb
+++ beer_overflow/app/controllers/comments_controller.rb
@@ -1,11 +1,12 @@
 class CommentsController < ApplicationController
+  before_action :set_comment, only: [:show, :destroy]
 
   def create
 
   end
 
   def new
-
+    @comment = Comment.new
   end
 
   def show
@@ -14,6 +15,16 @@ class CommentsController < ApplicationController
 
   def destroy
 
+  end
+
+  private
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content, :parent_id, :parent_type, :author_id)
   end
 
 end
