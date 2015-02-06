55e4337fdb7e04cd0d11ca86e676b9071b888a88
alexjtark <alex.tarkowski@gmail.com>
2015-02-06 12:09:17 -0500

    add question create method

--- beer_overflow/app/controllers/questions_controller.rb
+++ beer_overflow/app/controllers/questions_controller.rb
@@ -10,6 +10,15 @@ before_action :set_question, only: [:show, :edit, :destroy, :update]
     @question = Question.new
   end
 
+  def create
+    @question = Article.new(question_parameters_with_user_id)
+    if @question.save
+      redirect_to @question
+    else
+      redirect_to :back
+    end
+  end
+
   def edit
   end
 
@@ -27,6 +36,10 @@ before_action :set_question, only: [:show, :edit, :destroy, :update]
 
   def set_question
     @question = Question.find(params[:id])
+  end
+
+  def question_parameters_with_user_id
+    question_params.merge(user_id: session[:user_id])
   end
 
   def question_params
