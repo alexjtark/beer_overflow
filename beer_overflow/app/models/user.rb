18de48509cd4fd78acbc0bcef98e05e4bab00845
Ashley Baldwin-Hunter <ashleybaldwin-hunter@Ashleys-MacBook-Air.local>
2015-02-05 20:04:17 -0500

    update associations in user model

--- beer_overflow/app/models/user.rb
+++ beer_overflow/app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :email, presence: true, uniqueness: true
   validates :username, presence: true, uniqueness: true
-  has_many :questions
-  has_many :answers
-  has_many :comments
+  has_many :questions, foreign_key: :asker_id
+  has_many :answers, foreign_key: :responder_id
+  has_many :comments, foreign_key: :author_id
 end
