b9b82dcbb147fc6ddc27e176deb96213c8932b5f
Ashley Baldwin-Hunter <ashleybaldwin-hunter@Ashleys-MacBook-Air.local>
2015-02-05 23:22:50 -0500

    add destroy session and session_params methods to controller

--- beer_overflow/app/controllers/sessions_controller.rb
+++ beer_overflow/app/controllers/sessions_controller.rb
@@ -12,8 +12,14 @@ class SessionsController < ApplicationController
     end
   end
 
+  def destroy
+    reset_session
+    redirect_to root_path
+  end
 
+  private
 
-
-
+  def session_params
+    params.require(:session).permit(:email, :password)
+  end
 end
