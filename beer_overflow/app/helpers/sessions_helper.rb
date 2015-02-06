d394c931b40aaa7d08294de9a4b0eac7bce3bec9
Kevin Perumal <kevin.perumal@gmail.com>
2015-02-06 14:33:43 -0500

    debug auth issues and add logout button

--- beer_overflow/app/helpers/sessions_helper.rb
+++ beer_overflow/app/helpers/sessions_helper.rb
@@ -3,7 +3,8 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
-  def current_user
-    @current_user = @current_user || User.find_by(id: session[:user_id])
+   def current_user
+    @current_user = @current_user || User.find_by(id: session[:id])
   end
+
 end
