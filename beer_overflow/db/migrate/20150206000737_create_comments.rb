576f7af9dcec09a341a3de7d94df8f8e80e449c4
Ashley Baldwin-Hunter <ashleybaldwin-hunter@Ashleys-MacBook-Air.local>
2015-02-05 19:18:13 -0500

    add comment migration data

--- beer_overflow/db/migrate/20150206000737_create_comments.rb
+++ beer_overflow/db/migrate/20150206000737_create_comments.rb
@@ -1,6 +1,12 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
+      t.integer :parent_id
+      t.string :parent_type
+      t.text :content
+      t.integer :author_id
+
+      t.timestamps
     end
   end
 end
