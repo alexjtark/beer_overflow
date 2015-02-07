290e8131c7b218afb5bc87f1a4112399fb15e225
Ashley Baldwin-Hunter <ashleybaldwin-hunter@Ashleys-MacBook-Air.local>
2015-02-07 13:37:18 -0500

    create comments factories

--- beer_overflow/spec/factories/answer_comments.rb
+++ beer_overflow/spec/factories/answer_comments.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :answer_comment, class: "Comment" do
-    association :user
+    association :author, factory: :user
     association :answer
     content {Faker::Hacker.say_something_smart}
   end
--- beer_overflow/spec/factories/question_comments.rb
+++ beer_overflow/spec/factories/question_comments.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :question_comment, class: "Comment" do
-    association :user
+    association :author, factory: :user
     association :question
     content {Faker::Hacker.say_something_smart}
   end
