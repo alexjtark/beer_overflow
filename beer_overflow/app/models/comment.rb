bd3726a897d595c2ab85c83b54cb787ca6903f78
Ashley Baldwin-Hunter <ashleybaldwin-hunter@Ashleys-MacBook-Air.local>
2015-02-07 14:41:22 -0500

    create vote count method

--- beer_overflow/app/models/comment.rb
+++ beer_overflow/app/models/comment.rb
@@ -3,4 +3,10 @@ class Comment < ActiveRecord::Base
   belongs_to :author, class_name: "User"
   belongs_to :parent, polymorphic: true
   has_many :votes, as: :votable
+
+  def vote_count
+    up = votes.where(liked: true).count
+    down = (-1) * votes.where(liked: false).count
+    return (up + down)
+  end
 end
