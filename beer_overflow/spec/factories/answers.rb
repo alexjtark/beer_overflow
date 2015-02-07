ec3156017cd1204ca4a85cbef6aef1bddc29dc52
Ashley Baldwin-Hunter <ashleybaldwin-hunter@Ashleys-MacBook-Air.local>
2015-02-07 13:37:07 -0500

    create answers factory

--- beer_overflow/spec/factories/answers.rb
+++ beer_overflow/spec/factories/answers.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :answer do
-    association :user
+    association :responder, factory: :user
     association :question
     content {Faker::Lorem.paragraph}
   end
